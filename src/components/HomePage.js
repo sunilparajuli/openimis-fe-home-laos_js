bea7e0dc8505f9ec5a63437445ab1143cd8524b4
Eric Darchis <darchis@pobox.com>
2022-03-01 17:02:37 +0100

    v1.4.0-rc2

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-import { ProxyPage } from "@openimis/fe-core";
 
 
 class HomePage extends Component {
     render() {
-        return <ProxyPage url="/LegacyHome" />
+        // TODO: Make a more complete homepage
+        return <>
+        </>;
     }
 }
 
